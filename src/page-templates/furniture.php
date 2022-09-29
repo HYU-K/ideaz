b0c116099ca4bb226d825532734d926e8954f3d6
gou_keyy <lehuyhuy353@gmail.com>
2022-09-30 00:03:04 +0700

    huy add

---
+++ src/page-templates/furniture.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Template Name: Furniture
+ */
+
+get_header();
+?>
+    <main id="primary" class="site-main space-y-12">
+        <div class="container">
+            <div class="breadcrumb">
+                <a href="" title="">Trang chủ</a>
+                <a href="" title="">Nội thất rời</a>
+            </div>
+            <div class="html_launcher_products">
+                <div class="external-fixed">
+                    <div class="text-vertical" style="">
+                        <span class="color">IDEAZ</span><span> - nội thất rời</span>
+                    </div>
+                </div>
+                <div class="launcher_list">
+                    <a href="./chitiet-noithat.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="images">
+                                <div class="banner">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-1.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">SF-01</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-noithat.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="images">
+                                <div class="banner">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-2.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">SF-01</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-noithat.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="images">
+                                <div class="banner">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-3.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">SF-01</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-noithat.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="images">
+                                <div class="banner">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-4.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">SF-01</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-noithat.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="images">
+                                <div class="banner">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-5.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">SF-01</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-noithat.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="images">
+                                <div class="banner">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-6.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">SF-01</div>
+                        </div>
+                    </a>
+                </div>
+                <div id="image_number"></div>
+            </div>
+        </div>
+    </main>
+<?php
+get_footer();
---
+++ src/page-templates/texture-material.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Template Name: Light texture material
+ */
+
+get_header();
+?>
+    <main id="primary" class="site-main space-y-12">
+        <div class="banner-col" data-aos="zoom-in" data-aos-duration="1000">
+            <div class="bann_col slide_destinations">
+                <div class="bg">
+                    <img src='<?php echo get_theme_file_uri( '/images/banner.png') ?>' alt=''>
+                </div>
+                <div class="bg">
+                    <img src='<?php echo get_theme_file_uri( '/images/banner.png') ?>' alt=''>
+                </div>
+                <div class="bg">
+                    <img src='<?php echo get_theme_file_uri( '/images/banner.png') ?>' alt=''>
+                </div>
+            </div>
+            <span class="pagingInfo"></span>
+        </div>
+        <div class="html_material_col12">
+            <div class="container">
+                <div class="launcher_list">
+                    <a href="./chitiet-vatlieu.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="banner">
+                                <div class="images">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-1.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">Invitrum Valcucine</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-vatlieu.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="banner">
+                                <div class="images">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-2.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">Invitrum Valcucine</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-vatlieu.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="banner">
+                                <div class="images">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-3.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">Invitrum Valcucine</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-vatlieu.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="banner">
+                                <div class="images">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-4.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">Invitrum Valcucine</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-vatlieu.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="banner">
+                                <div class="images">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-5.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">Invitrum Valcucine</div>
+                        </div>
+                    </a>
+                    <a href="./chitiet-vatlieu.html" title="" class="">
+                        <div class="item-banner wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="banner">
+                                <div class="images">
+                                    <img src='<?php echo get_theme_file_uri( '/images/kitchens-6.png') ?>' alt=''>
+                                </div>
+                            </div>
+                            <div class="title_project title20">Invitrum Valcucine</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php
+get_footer();
