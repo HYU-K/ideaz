aca48e59108a54a398cd59af713aae0b0cb61b10
gou_keyy <lehuyhuy353@gmail.com>
2022-09-30 08:19:37 +0700

    huy add

---
+++ src/page-templates/idea.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Idea ideaz
+ */
+
+get_header();
+?>
+    <main id="primary" class="site-main space-y-12">
+        <div class="html_technology-col12">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="" title="">Trang chủ</a>
+                    <a href="" title="">Ý tưởng ideaz</a>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php
+get_footer();
